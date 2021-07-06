7020cc15a54caee6c760b83e7116b1d9e22c04c0
William Brawner <me@wbrawner.com>
2021-07-06 09:48:48 -0600

    fixup! fixup! Add ktor dependencies and basic server

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     java
     kotlin("jvm")
     id("org.springframework.boot")
+    application
 }
 
 apply(plugin = "io.spring.dependency-management")
@@ -24,6 +25,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-cio:$ktorVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
@@ -38,10 +40,14 @@ description = "twigs-server"
 
 val twigsMain = "com.wbrawner.twigs.server.TwigsServerApplication"
 
+application {
+    mainClass.set(twigsMain)
+}
+
 tasks.bootJar {
     mainClassName = twigsMain
 }
 
 tasks.bootRun {
-    main = twigsMain
+    mainClass.set(twigsMain)
 }
