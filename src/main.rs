6bbb421167a6a297f1d64a42ec29d66150110c93
William Brawner <me@wbrawner.com>
2023-10-18 19:48:49 -0600

    Add actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn pong() -> impl Responder {
+    HttpResponse::Ok().body("PONG")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port: u16 = env::var("PORT").map_or(8080, |p| p.parse::<u16>().unwrap_or(8080));
+
+    HttpServer::new(|| App::new().route("/ping", web::get().to(pong)))
+        .bind(("0.0.0.0", port))?
+        .run()
+        .await
 }
