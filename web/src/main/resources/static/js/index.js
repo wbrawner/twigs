a2579dbd6dcc004895e8bb3c0afdffd04ac78ffb
William Brawner <me@wbrawner.com>
2024-03-30 11:30:10 -0600

    Fix form disabling upon submission
    Turns out if you disable everything then nothing is sent!

--- web/src/main/resources/static/js/index.js
+++ web/src/main/resources/static/js/index.js
@@ -3,9 +3,6 @@ const forms = document.getElementsByTagName('form')
 for (let i = 0; i < forms.length; i++) {
     const form = forms[i]
     form.onsubmit = () => {
-        const inputs = form.getElementsByTagName('input')
-        for (let j = 0; j < inputs.length; j++) {
-            inputs[j].disabled = true
-        }
+        form.querySelector('input[type="submit"]').disabled = true
     }
 }
