0fd0b16b4cb4b5287580b85575a354dbea707e86
William Brawner <me@wbrawner.com>
2024-07-09 17:27:04 -0600

    Improve sidebar handling when JavaScript is enabled

--- web/src/main/resources/static/js/index.js
+++ web/src/main/resources/static/js/index.js
@@ -6,3 +6,18 @@ for (let i = 0; i < forms.length; i++) {
         form.querySelector('input[type="submit"]').disabled = true
     }
 }
+
+const sidebar = document.querySelector('#sidebar')
+
+document.querySelector('#hamburger').onclick = (e) => {
+    e.preventDefault()
+    sidebar.style.transform = 'translateX(0)'
+}
+
+document.addEventListener('click', (e) => {
+    const style = window.getComputedStyle(sidebar)
+    const matrix = new DOMMatrixReadOnly(style.getPropertyValue("transform"))
+    if (matrix.m41 === 0) {
+        sidebar.style.transform = 'translateX(-100%)'
+    }
+})
